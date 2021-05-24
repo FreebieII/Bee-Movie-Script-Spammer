fa7548f59491a2df1675057e3b567c5467f39e9e
Freebie <69956546+FreebieII@users.noreply.github.com>
2021-05-24 15:18:19 +0200

    Update spam.py

--- spam.py
+++ spam.py
@@ -5,4 +5,4 @@ time.sleep(5) # starts running in 5 seconds
 s = open("beemovie", "r") #Opens beemovie file 
 for word in s:
     pyautogui.typewrite(word) #writes the word per line
-    pyautogui.press("enter") #presses enter
+    pyautogui.press("enter") #press enter
